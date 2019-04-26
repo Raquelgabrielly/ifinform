0124f697a62c28cd03e1ba11536e37dcd446c4f7
AlissonThyago <48837955+AlissonThyago@users.noreply.github.com>
2019-04-26 14:12:24 -0300

    banco de dados att

--- db1.sql
+++ db1.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 05-Abr-2019 às 16:26
--- Versão do servidor: 10.1.38-MariaDB
--- versão do PHP: 7.3.3
+-- Generation Time: 26-Abr-2019 às 19:16
+-- Versão do servidor: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -33,18 +33,23 @@ CREATE TABLE `alunos` (
   `nome` varchar(90) COLLATE utf8_unicode_ci NOT NULL,
   `matricula` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(70) COLLATE utf8_unicode_ci NOT NULL,
-  `telefone` varchar(13) COLLATE utf8_unicode_ci NOT NULL
+  `telefone` varchar(13) COLLATE utf8_unicode_ci NOT NULL,
+  `senha` varchar(30) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `alunos`
 --
 
-INSERT INTO `alunos` (`id`, `nome`, `matricula`, `email`, `telefone`) VALUES
-(3, 'Filipe', '20171infig0', 'fizidorio7@gmail.com', '0'),
-(4, 'Leonardo', '0001010', 'leo@leo.com', '1234567897'),
-(5, 'Alisson', '01010101', 'alisson@als.com', '81818181'),
-(6, 'nao sei', '1010', 'naosei@ns.com', '81818181');
+INSERT INTO `alunos` (`id`, `nome`, `matricula`, `email`, `telefone`, `senha`) VALUES
+(4, 'João', '0001010', 'leo@leo.com', '1234567897', ''),
+(5, 'Alisson', '01010101', 'alisson@als.com', '81818181', ''),
+(6, 'nao sei', '1010', 'naosei@ns.com', '81818181', ''),
+(7, 'Filipe', '20171infig0', 'fizidorio7@gmail.com', '81 9 92341017', ''),
+(8, 'Filipe', '20171infig0', 'fizidorio7@gmail.com', '81 9 92341017', ''),
+(9, 'Morgana Fernandes', 'infig0148', 'anagrom1999@gmail.com', '81998977870', ''),
+(10, '', '', '', '', ''),
+(11, 'teste', 'teste', 'teste@teste.com', '123456', '123333');
 
 --
 -- Indexes for dumped tables
@@ -64,7 +69,7 @@ ALTER TABLE `alunos`
 -- AUTO_INCREMENT for table `alunos`
 --
 ALTER TABLE `alunos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
