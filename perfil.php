625f99bc5dee8526c2a0772d5a7e905f4dc2c553
Luciano Junior <49078083+scofieldjr@users.noreply.github.com>
2019-05-03 14:14:34 -0300

    perfil do aluno

---
+++ perfil.php
@@ -0,0 +1,54 @@
+<?php  
+//header
+  include_once 'includes/header.php';
+  //mensagem de cadastrado ou nao.
+  include_once 'includes/mensagem.php';
+?>  
+
+    
+
+      <h3 class="light">Perfil do Aluno</h3>
+      <div class="col s12 m8 offset-m2 l6 offset-l3">
+        <div class="card-panel grey lighten-5 z-depth-1">
+          <div class="row valign-wrapper">
+            <div class="col s2">
+              <img src="foto.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+              <span class="black-text">
+                Informações sobre seu perfil
+              </span>
+       <ul class="collapsible">
+  <li>
+    <div class="collapsible-header">
+      <i class="material-icons">border_color</i>
+      Nome
+      <span class="new badge">1</span></div>
+    <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+  </li>
+  <li>
+    <div class="collapsible-header">
+      <i class="material-icons">place</i>
+      Cidade
+      <span class="badge">1</span></div>
+    <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+  </li>
+  <li>
+    <div class="collapsible-header">
+      <i class="material-icons">call</i>
+      Telefone
+      <span class="badge">3</span></div>
+    <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+  </li>
+</ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  </div>
+ </div>
+<?php 
+  //footer
+  include_once 'includes/footer.php';
+?>
