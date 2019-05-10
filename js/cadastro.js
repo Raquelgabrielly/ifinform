96a42d70ee598ab372d8617727bf2957aead2f28
Filipe Izidorio <31548630+lipeh777@users.noreply.github.com>
2019-05-10 03:29:02 -0300

    adicionando o cadastro.js

---
+++ js/cadastro.js
@@ -0,0 +1,48 @@
+function get(id){
+	return document.getElementById(id);
+	}
+
+	var perfilDiscente = get('perfil-discente');
+	var perfilDocente = get('perfil-docente');
+	var perfilVisitante = get('perfil-visitante');
+
+	var icone = get('icone');
+	var input = get('perfil');
+	var label = get('label');
+
+
+	perfilDiscente.addEventListener('click', function add(evt) {
+			evt.preventDefault();
+			input.type = 'text';
+			input.name = 'matricula';
+			input.removeAttribute("disabled");
+			input.removeAttribute("value");
+			input.id = 'matricula';
+			label.for ='matricula';
+			label.innerHTML = 'Matrícula';
+			icone.innerHTML = 'power_input';
+	});
+
+	perfilDocente.addEventListener('click', function add1(evt) {
+			evt.preventDefault();
+			input.type = 'text';
+			input.name = 'siape';
+			input.removeAttribute("disabled");
+			input.removeAttribute("value");
+			input.id = 'siape';
+			label.for ='siape';
+			label.innerHTML = 'Siape';
+			icone.innerHTML = 'power_input';
+	});
+
+	perfilVisitante.addEventListener('click', function add2(evt) {
+			evt.preventDefault();
+			input.type = 'text';
+			input.name = 'cpf';
+			input.removeAttribute("disabled");
+			input.removeAttribute("value");
+			input.id = 'cpf';
+			label.for ='cpf';
+			label.innerHTML = 'CPF';
+			icone.innerHTML = 'power_input';
+	});
