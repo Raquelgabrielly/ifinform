9ab083586b0e2283bfc7fffe8aa90ce11b4b57ec
Morgana Fernandes <48961500+K0rgana@users.noreply.github.com>
2019-05-17 16:29:27 -0300

    correção de variaves2

--- php_action/create.php
+++ php_action/create.php
@@ -90,8 +90,8 @@ elseif (isset($_POST['cpf'])) {
 	$senha = mysqli_escape_string($connect, $_POST['senha']);
 
 	// testa se os dados ja existem como usuario (visitante)
-	$query1 = "SELECT * FROM discente WHERE VISITANTE_CPF = '$cpf'";
-	$query2 = "SELECT * FROM discente WHERE VISITANTE_EMAIL = '$email'";
+	$query1 = "SELECT * FROM visitante WHERE VISITANTE_CPF = '$cpf'";
+	$query2 = "SELECT * FROM visitante WHERE VISITANTE_EMAIL = '$email'";
 	$result1 = mysqli_query($connect, $query1);
 	$result2 = mysqli_query($connect, $query2);
 	$row1 = mysqli_num_rows($result1);
