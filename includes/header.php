eb6dd57d56df7cc0c55caa6c6e34431747ad202e
Filipe Izidorio <31548630+lipeh777@users.noreply.github.com>
2019-05-29 01:57:12 -0300

    produzindo a pagina de agendamento

--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,7 @@
         <script type="text/javascript" src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="../css/style.css">
     </head>
 
     <body>
