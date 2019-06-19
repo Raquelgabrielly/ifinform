ff3d91ba6e0aa3bed7df82fd2a6d5c4144ef32f4
Filipe Izidorio <31548630+lipeh777@users.noreply.github.com>
2019-06-19 04:39:39 -0300

    commit final

--- perfil-aluno.php
+++ perfil-aluno.php
@@ -10,55 +10,53 @@
     
 
 <h3 class="light"> Olá <?= $_SESSION['nome'] ?></h3>
-<div class="col s12 m8 offset-m2 l6 offset-l3">
-  <div class="card-panel grey lighten-5 z-depth-1">
-    <div class="row valign-wrapper">
-      <div class="col s2">
-        <img src="foto.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
-      </div>
-      <div class="col s10">
-        <span class="black-text">
-            Informações sobre seu perfil
-        </span>
-        <ul class="collapsible">
-          <li>
-            <div class="collapsible-header">
-            <i class="material-icons">border_color</i>
-            Nome
-            <span class="badge"></span>
-            </div>
-          <div class="collapsible-body">
-            <p><?= $_SESSION['nome'] ?></p>
-          </div>
-          </li>
-          <li>
-            <div class="collapsible-header">
-              <i class="material-icons">place</i>
-              Seu telefone
-              <span class="badge"></span>
-            </div>
-            <div class="collapsible-body">
-              <p><?= $_SESSION['telefone'] ?></p>
-            </div>
-          </li>
-          <li>
-            <div class="collapsible-header">
-              <i class="material-icons">call</i>
-              Matrícula
-              <span class="badge"></span>
-            </div>
-            <div class="collapsible-body">
-              <p><?= $_SESSION['matricula'] ?></p>
-            </div>
-          </li>
-        </ul>
+
+<?php 
+
+  $query = "SELECT * FROM agendamento ORDER BY AGENDADO_PARA DESC";
+  $result = mysqli_query($connect, $query);
+
+  while($dados = mysqli_fetch_assoc($result)):
+    if ($dados['DISCENTE_ID'] == $_SESSION['id']):
+      
+ ?>
+
+
+
+<div class="row">
+    <div class="col s12 m6 push-m3">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title"><?= $dados['NOME'] ?></span>
+          <?php 
+
+            $datahora = explode(' ', $dados['AGENDADO_PARA']);  
+            $hora = $datahora[1];
+
+            $dataformat = explode('-', $datahora[0]);
+            $data = implode('/', [$dataformat[2], $dataformat[1], $dataformat[0]]);
+
+           ?>
+          <p>Está agendado para <?= $data ?> </p>
+          <p>Às <?= $hora ?> </p><br>
+
+          <span class="card-title">Assunto</span>
+
+          <p><?= $dados['ASSUNTO'] ?></p>
+        </div>
+        <div class="card-action">
+          <!-- <a href="#">This is a link</a>
+          <a href="#">This is a link</a> -->
+        </div>
       </div>
     </div>
   </div>
-</div>
 
-</div>
-</div>
+<?php endif; ?>
+<?php endwhile; ?>
+
+
+
 
 
 <!-- Agendamento -->
@@ -81,6 +79,18 @@
               <?php endwhile; ?>
               <!-- faltando a logica da programação em php no arquivo: php_action/agendamento.php -->
           </select>
+          
+          <?php 
+            $query = "SELECT * FROM docente";
+            $result = mysqli_query($connect, $query);
+
+            while($dados = mysqli_fetch_assoc($result)):
+          ?>
+            
+            <input type="hidden" value="<?= $dados['DOCENTE_NOME'] ?>" name="nome">
+
+          <?php endwhile; ?>
+
         </div>
         <div class="input-field col s12">
           <input type="text" class="datepicker" name="data" id="data">
